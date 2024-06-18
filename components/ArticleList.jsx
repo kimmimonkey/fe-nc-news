14eb39c1fd07967d5e6fd3840146782116c2d28f
kimmimonkey <kim_muncey@hotmail.com>
2024-06-18 11:26:12 +0100

    exports requests to API to another file, refactors using axios, reformats date

--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
+import { getArticles } from "../src/api"
 import ArticleCard from "./ArticleCard"
 
 
-const ArticleList= () => {
+const ArticleList = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const [list , setList] = useState([]);
+    const [list, setList] = useState([]);
 
+useEffect(() => {
+    getArticles() 
+    .then((articles) => {
+        setList(articles);
+        setIsLoading(false);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}, [])
 
-
-    useEffect(() => {
-        console.log("useEffect triggered!")
-        fetch("https://nc-news-zcdp.onrender.com/api/articles")
-        .then((response) => response.json())
-        .then((body) => {
-            setList(body.articles);
-            setIsLoading(false);
-        });
-    }, []);
-
+    
 if (isLoading) {
     return <p> Loading... </p>
 }
 
-    return (
-        <section> 
+return (
+    <section>
         <h2> NC Articles </h2>
         <ul>
-            {list.map((article) => <ArticleCard key = {article.article_id}
-            title = {article.title}
-            topic = {article.topic}
-            author = {article.author}
-            published = {article.created_at}
-            img = <img src = {article.article_img_url} alt = {article.title} width = "100" height = "100"/>
-            comments = {article.comment_count} /> )}
+            {list.map((article) => <ArticleCard key={article.article_id}
+                title={article.title}
+                topic={article.topic}
+                author={article.author}
+                published= {new Date(article.created_at).toUTCString()}
+                img=<img src={article.article_img_url} alt={article.title} width="100" height="100" />
+                comments={article.comment_count} />)}
         </ul>
-        </section>
-    )
+    </section>
+)
 
 
 
     }
 
-    export default ArticleList;
+export default ArticleList;
---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header= () => { 
+    return (
+        <section> 
+        <h1> NC News </h1>
+        <h2> Welcome to the home of all things NC News!</h2>
+        </section>
+    )
+}
+
+export default Header; 
